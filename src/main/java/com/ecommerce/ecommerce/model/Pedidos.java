7da1c8b81c4c9f4adee25dfdc0fe94fc7726c3b9
Roberta <99450524+RabeloRoberta@users.noreply.github.com>
2022-06-07 20:27:47 -0300

    alteração pedidos

--- src/main/java/com/ecommerce/ecommerce/model/Pedidos.java
+++ src/main/java/com/ecommerce/ecommerce/model/Pedidos.java
@@ -3,8 +3,10 @@ package com.ecommerce.ecommerce.model;
 import java.util.List;
 import java.util.UUID;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -12,9 +14,11 @@ import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 
 @Entity
@@ -30,39 +34,21 @@ public class Pedidos {
 	@Column(name = "id_pedido")
 	private Long idPedido;
 			
-	@Column(name = "qnt_produto")
-	private int qntProdutos;
+	@Column(name = "mais_vendidos")
+	private int maisVendidos;
 	
 	@NotNull
 	@Column(name = "valor_total")
 	private double valorTotal;
-	
-	@NotNull
-	@Column(name = "confirmacao_pagamento")
-	private int confirmacaoPagamento;
-	
-	@NotNull
-	@GeneratedValue(strategy = GenerationType.AUTO)
-	@Column(name= "chave_acesso", unique = true)	
-	private UUID chaveAcesso ;
-	
-	@NotNull
-	@Column(name= "confirmacao_envio")
-	private int confirmacaoEnvio;
-	
-	
-	@ManyToOne
+		
+	@ManyToOne 
 	@JoinColumn(name= "usuarios_id")
-	@JsonManagedReference
 	private Usuarios usuarios;
 	
-	@ManyToMany
-	@JoinTable(name= "pedidos_produtos",
-	joinColumns = @JoinColumn(name = "pedido_id"),
-	inverseJoinColumns= @JoinColumn(name = "produto_id")) //tabela intermediária para guardar quais produtos fazem parte dos pedidos
-	@JsonManagedReference
+	@OneToMany (mappedBy = "pedidos", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+	@JoinTable(name= "id_produtos")
+	@JsonIgnore
 	private List<Produtos> produtos;
-	
 
 	public Long getIdPedido() {
 		return idPedido;
@@ -72,12 +58,12 @@ public class Pedidos {
 		this.idPedido = idPedido;
 	}
 
-	public int getQntProdutos() {
-		return qntProdutos;
+	public int getMaisVendidos() {
+		return maisVendidos;
 	}
 
-	public void setQntProdutos(int qntProdutos) {
-		this.qntProdutos = qntProdutos;
+	public void setMaisVendidos(int maisVendidos) {
+		this.maisVendidos = maisVendidos;
 	}
 
 	public double getValorTotal() {
@@ -88,36 +74,11 @@ public class Pedidos {
 		this.valorTotal = valorTotal;
 	}
 
-	public int getConfirmacaoPagamento() {
-		return confirmacaoPagamento;
-	}
-
-	public void setConfirmacaoPagamento(int confirmacaoPagamento) {
-		this.confirmacaoPagamento = confirmacaoPagamento;
-	}
-
-	@SuppressWarnings("static-access") //gerar chave de acesso aleatória
-	public UUID getChaveAcesso() {
-		return chaveAcesso.randomUUID();
-	}
-
-	public void setChaveAcesso(UUID chaveAcesso) {
-		this.chaveAcesso = chaveAcesso;
-	}
-
-	public int getConfirmacaoEnvio() {
-		return confirmacaoEnvio;
-	}
-
-	public void setConfirmacaoEnvio(int confirmacaoEnvio) {
-		this.confirmacaoEnvio = confirmacaoEnvio;
-	}
-
 	public Usuarios getUsuarios() {
 		return usuarios;
 	}
 
-	public void setClientes(Usuarios usuarios) {
+	public void setUsuarios(Usuarios usuarios) {
 		this.usuarios = usuarios;
 	}
 
@@ -129,5 +90,11 @@ public class Pedidos {
 		this.produtos = produtos;
 	}
 	
-		
+	
 }
+
+
+
+	
+
+	
